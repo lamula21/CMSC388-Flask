b8201cee90f8c01619a526172439de811c1b29c5
lamula21 <jvaldiv8@montgomerycollege.edu>
2023-03-16 14:17:06 -0400

    done prj3

--- p3/flask_app/app.py
+++ p3/flask_app/app.py
@@ -11,8 +11,8 @@ from flask_app.forms import SearchForm, MovieReviewForm
 from flask_app.model import MovieClient
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = ""
-app.config['SECRET_KEY'] = ""
+app.config["MONGO_URI"] = "mongodb://127.0.0.1:27017/mydatabase"
+app.config['SECRET_KEY'] = "b'\xddgY\xe3\xb4^P?IW\xb7u\xf8L\xd6\x95'"
 
 app.config.update(
     SESSION_COOKIE_HTTPONLY=True,
@@ -35,11 +35,53 @@ def index():
 
 @app.route('/search-results/<query>', methods=['GET'])
 def query_results(query):
-    return 'Query'
+    client = MovieClient(os.environ.get('OMDB_API_KEY'))
+    results = client.search(query)
+    message = None
+    if results is None:
+        return render_template('query_results.html', error_msg=message)
+    return render_template('query_results.html', results=results)
+    
 
 @app.route('/movies/<movie_id>', methods=['GET', 'POST'])
 def movie_detail(movie_id):
-    return 'movie_detail'
+    movie_client = MovieClient(os.environ.get('OMDB_API_KEY'))
+    
+    try:
+        movie = movie_client.retrieve_movie_by_id(movie_id)
+    except Exception as e:
+        return render_template('movie_detail.html', error_msg=str(e))
+
+    #reviews = list(mongo.db.reviews.find({'imdb_id': movie.imdb_id}))
+
+    form = MovieReviewForm()
+
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            commenter = form.name.data
+            content = form.text.data
+            date = current_time()
+            imdb_id = movie.imdb_id
+
+
+
+            # New document is created inside
+            new_review = {
+                'imdb_id':imdb_id,
+                'commenter':commenter,
+                'content':content,
+                'date':date
+                }
+            
+
+            # Create document for our collection "reviews", then insert data into our database
+            mongo.db.reviews.insert_one(new_review)
+            return redirect(url_for('movie_detail', movie_id=movie.imdb_id))
+        else:
+            print('validation failed')
+    # Retrieve 'reviews' from db and display it on movie_detail.html
+    reviews = list(mongo.db.reviews.find({'imdb_id': movie.imdb_id}))
+    return render_template('movie_detail.html', movie=movie, reviews=reviews, form=form)           
 
 # Not a view function, used for creating a string for the current time.
 def current_time() -> str:
--- p3/flask_app/forms.py
+++ p3/flask_app/forms.py
@@ -1,9 +1,16 @@
 from flask_wtf import FlaskForm
-# from wtforms import 
-# from wtforms.validators import
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import Length, InputRequired
 
 class SearchForm(FlaskForm):
-    pass
+    search_query = StringField('query', validators=[InputRequired(), Length(min=1, max=30, message='Must be at least 1 character long and no more than 30 characteres')])
+    
+    submit = SubmitField('btn btn-outline-success')
+
 
 class MovieReviewForm(FlaskForm):
-    pass
+    name = StringField('Name or Alias', validators=[InputRequired(), Length(min=1, max=50)])
+
+    text = TextAreaField('Comment', validators=[Length(min=1, max=500)])
+
+    submit = SubmitField()
